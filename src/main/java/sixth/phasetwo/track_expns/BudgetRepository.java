88da6767471f5638c3c8bcdf5b88df9fca4dcc64
Sannith9598 <sannithsanni2005@gmail.com>
2025-06-05 10:13:06 +0530

    deleting some  of entity and repo

--- src/main/java/sixth/phasetwo/track_expns/BudgetRepository.java
+++
@@ -1,9 +0,0 @@
-package sixth.phasetwo.track_expns;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BudgetRepository extends JpaRepository<BudgetEntity, Integer> {
-    List<BudgetEntity> findByType(String type);
-}
--- src/main/java/sixth/phasetwo/track_expns/ExpenseEntity.java
+++ src/main/java/sixth/phasetwo/track_expns/ExpenseEntity.java
@@ -3,9 +3,8 @@ package sixth.phasetwo.track_expns;
 public class ExpenseEntity {
     private int id;
     private String type;
-    private int budetId;
     private double amount;
-
+    
     public int getId() {
         return id;
     }
@@ -18,16 +17,12 @@ public class ExpenseEntity {
     public void setType(String type) {
         this.type = type;
     }
-    public int getBudetId() {
-        return budetId;
-    }
-    public void setBudetId(int budetId) {
-        this.budetId = budetId;
-    }
     public double getAmount() {
         return amount;
     }
     public void setAmount(double amount) {
         this.amount = amount;
     }
+
+   
 }
